2ef9a08a5ea5fdc7b67a096bc8bee2f0ac4d711c
Arachi10 <noraarachi10@gmail.com>
2023-07-24 04:18:05 -0700

    _printf.c

--- _printf.c
+++ _printf.c
@@ -1,67 +1,42 @@
-
 #include "main.h"
-
-void print_buffer(char buffer[], int *buff_ind);
-
 /**
-* _printf - Printf function
-* @format: format.
-* Return: Printed chars.
-*/
+  *_printf - prints the arguments passed through to it
+  *@format: the character string
+  *Return: always integer
+  */
 int _printf(const char *format, ...)
 {
-int i, printed = 0, printed_chars = 0;
-int flags, width, precision, size, buff_ind = 0;
-va_list list;
-char buffer[BUFF_SIZE];
+	va_list all;
+	int i = 0, count = 0;
 
-if (format == NULL)
-return (-1);
+	va_start(all, format);
+	for (; format[i] != '\0'; i++)
+	{
+		if (format[i] != '%')
+		{
+			write_char(format[i]);
+		}
+		else if (format[i + 1] == 'c')
+		{
+			write_char(va_arg(all, int));
+			i++;
+		}
+		else if (format[i + 1] == '%')
+		{
+			write_char('%');
+			i++;
+		}
+		else if (format[i + 1] == 's')
+		{
+			char *s = va_arg(all, char*);
 
-va_start(list, format);
-
-for (i = 0; format && format[i] != '\0'; i++)
-{
-if (format[i] != '%')
-{
-buffer[buff_ind++] = format[i];
-if (buff_ind == BUFF_SIZE)
-print_buffer(buffer, &buff_ind);
-/* write(1, &format[i], 1);*/
-printed_chars++;
-}
-else
-{
-print_buffer(buffer, &buff_ind);
-flags = get_flags(format, &i);
-width = get_width(format, &i, list);
-precision = get_precision(format, &i, list);
-size = get_size(format, &i);
-++i;
-printed = handle_print(format, &i, list, buffer,
-flags, width, precision, size);
-if (printed == -1)
-return (-1);
-printed_chars += printed;
-}
+			count = pu_ts(s);
+			i++;
+			count += (count - 1);
+		}
+			count += 1;
+	va_end(all);
+	}
+	return (count);
 }
 
-print_buffer(buffer, &buff_ind);
-
-va_end(list);
-
-return (printed_chars);
-}
-
-/**
-* print_buffer - Prints the contents of the buffer if it exist
-* @buffer: Array of chars
-* @buff_ind: Index at which to add next char, represents the length.
-*/
-void print_buffer(char buffer[], int *buff_ind)
-{
-if (*buff_ind > 0)
-write(1, &buffer[0], *buff_ind);
-
-*buff_ind = 0;
-}
